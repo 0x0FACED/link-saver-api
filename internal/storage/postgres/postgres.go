8ef2ca9090ba39bdcce340af07bdfd55ace91c0f
0x0FACED <https.genius@gmail.com>
2024-08-17 15:15:48 +0300

    added: config field to Postgres struct, getConnStr() method

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -2,18 +2,21 @@ package postgres
 
 import (
 	"database/sql"
+	"fmt"
 
+	"github.com/0x0FACED/link-saver-api/config"
 	"github.com/0x0FACED/link-saver-api/internal/domain/models"
 	"github.com/0x0FACED/link-saver-api/internal/storage"
 	_ "github.com/lib/pq"
 )
 
 type Postgres struct {
-	db *sql.DB
+	db     *sql.DB
+	Config config.DatabaseConfig
 }
 
-func (p *Postgres) Connect(connStr string) error {
-	db, err := sql.Open("postgres", connStr)
+func (p *Postgres) Connect() error {
+	db, err := sql.Open("postgres", p.getConnStr())
 	if err != nil {
 		return storage.ErrConnectDB
 	}
@@ -27,15 +30,16 @@ func (p *Postgres) Connect(connStr string) error {
 	return nil
 }
 
+func (p *Postgres) getConnStr() string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		p.Config.Username, p.Config.Password, p.Config.Host, p.Config.Port, p.Config.Name)
+}
+
 func (p *Postgres) SaveLink() error {
 	panic("not implemented") // TODO: Implement
 }
 
-func (p *Postgres) GetLinkByDescription(desc string) (models.Link, error) {
-	panic("not implemented") // TODO: Implement
-}
-
-func (p *Postgres) GetLinksByUsername(username string) ([]models.Link, error) {
+func (p *Postgres) GetLinksByUsernameDesc(username string, desc string) ([]models.Link, error) {
 	panic("not implemented") // TODO: Implement
 }
 
