5c8f6274a8ed6d02744296bb07be63a410005359
0x0FACED <https.genius@gmail.com>
2024-08-17 15:13:54 +0300

    upd: server struct, added New() func

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,20 +1,49 @@
 package server
 
 import (
+	"github.com/0x0FACED/link-saver-api/config"
+	"github.com/0x0FACED/link-saver-api/internal/storage"
+	"github.com/0x0FACED/link-saver-api/internal/storage/postgres"
+	"github.com/0x0FACED/proto-files/link_service/gen"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type server struct {
-	r *echo.Echo
+	config config.ServerConfig
+	db     storage.Database
+	gen.UnimplementedLinkServiceServer
+
+	echo *echo.Echo
 }
 
-func New() *server {
+func New(cfg *config.Config) *server {
+
+	// TODO: add more drivers
+	var db storage.Database
+	switch cfg.Database.Driver {
+	case "postgres":
+		db = &postgres.Postgres{Config: cfg.Database}
+	default:
+		db = &postgres.Postgres{Config: cfg.Database}
+	}
+
+	if db.Connect() == storage.ErrConnectDB {
+		panic("cant connect db, panic!")
+	}
 
 	return &server{
-		r: echo.New(),
+		config: cfg.Server,
+		echo:   configureRouter(),
+		db:     db,
 	}
 }
 
-func (s *server) Get() {
+func configureRouter() *echo.Echo {
+	e := echo.New()
 
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	return e
 }
