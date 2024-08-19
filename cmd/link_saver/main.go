61338a82c563b9bb7543fd87a5e9da882fa33ea2
0x0FACED <https.genius@gmail.com>
2024-08-19 16:42:49 +0300

    upd: start moved to server

--- cmd/link_saver/main.go
+++ cmd/link_saver/main.go
@@ -1,28 +1,11 @@
 package main
 
 import (
-	"log"
-	"net"
-
-	"github.com/0x0FACED/link-saver-api/config"
 	"github.com/0x0FACED/link-saver-api/internal/server"
-	"github.com/0x0FACED/proto-files/link_service/gen"
-	"google.golang.org/grpc"
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":50051")
-	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
-	}
-	s := grpc.NewServer()
-	cfg, err := config.Load()
-	if err != nil {
-		panic("cant load config, panic!")
-	}
-	srv := server.New(cfg)
-	gen.RegisterLinkServiceServer(s, srv)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
+	if err := server.Start(); err != nil {
+		panic("cant start server: " + err.Error())
 	}
 }
