b427772c8878babd2fc93decd49d6b0df5b21a2f
0x0FACED <https.genius@gmail.com>
2024-08-19 16:46:20 +0300

    added: UserWorker intfc, custom errors

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,23 +1,36 @@
 package storage
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 
 	"github.com/0x0FACED/link-saver-api/internal/domain/models"
 )
 
 var (
-	ErrConnectDB = errors.New("err connect db")
+	ErrConnectDB      = errors.New("err connect db")
+	ErrUserNotFound   = errors.New("user not found")
+	ErrLinksNotFound  = errors.New("links not found")
+	ErrNoRowsAffected = errors.New("no rows affected")
 )
 
 type Database interface {
 	Connect() error
 
 	LinkWorker
+	UserWorker
+}
+
+type UserWorker interface {
+	SaveUser(ctx context.Context, tx *sql.Tx, u *models.User) (int, error)
+	GetUserByUsername(ctx context.Context, tx *sql.Tx, username string) (*models.User, error)
+	GetUserIDByUsername(ctx context.Context, tx *sql.Tx, username string) (int, error)
 }
 
 type LinkWorker interface {
-	SaveLink() error
-	GetLinksByUsernameDesc(username string, desc string) ([]models.Link, error)
-	DeleteLink() error
+	SaveLink(ctx context.Context, l *models.Link) error
+	GetUserLinks(ctx context.Context, username string) ([]models.Link, error)
+	GetLinksByUsernameDesc(ctx context.Context, username string, desc string) ([]models.Link, error)
+	DeleteLink(ctx context.Context, l *models.Link) error
 }
