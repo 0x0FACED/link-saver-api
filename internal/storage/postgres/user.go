c58b5be6a11a5dbae592c4c111d3dde4b530091b
0x0FACED <https.genius@gmail.com>
2024-08-19 16:48:01 +0300

    added: UserWorker intfc methods impls

---
+++ internal/storage/postgres/user.go
@@ -0,0 +1,67 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"log"
+
+	"github.com/0x0FACED/link-saver-api/internal/domain/models"
+	"github.com/0x0FACED/link-saver-api/internal/storage"
+)
+
+func (p *Postgres) SaveUser(ctx context.Context, tx *sql.Tx, u *models.User) (int, error) {
+	var err error
+	var id int
+	q := `INSERT INTO users (username) VALUES ($1) RETURNING id`
+	if tx != nil {
+		err = tx.QueryRowContext(ctx, q, u.UserName).Scan(&id)
+	} else {
+		err = p.db.QueryRowContext(ctx, q, u.UserName).Scan(&id)
+	}
+	if err != nil {
+		log.Println("[DB] error SaveUser():", err)
+		return -1, err
+	}
+
+	return id, nil
+}
+
+func (p *Postgres) GetUserByUsername(ctx context.Context, tx *sql.Tx, username string) (*models.User, error) {
+	var u models.User
+	var err error
+	q := `SELECT id, username FROM users WHERE username = $1`
+	if tx != nil {
+		err = tx.QueryRowContext(ctx, q, username).Scan(&u.ID, &u.UserName)
+	} else {
+		err = p.db.QueryRowContext(ctx, q, username).Scan(&u.ID, &u.UserName)
+	}
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, storage.ErrUserNotFound
+		}
+		log.Println("[DB] error GetUserByUsername():", err)
+		return nil, err
+	}
+	return &u, nil
+}
+
+func (p *Postgres) GetUserIDByUsername(ctx context.Context, tx *sql.Tx, username string) (int, error) {
+	var id int
+	var err error
+	q := `SELECT id FROM users WHERE username = $1`
+	if tx != nil {
+		err = tx.QueryRowContext(ctx, q, username).Scan(&id)
+	} else {
+		err = p.db.QueryRowContext(ctx, q, username).Scan(&id)
+	}
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return -1, storage.ErrUserNotFound
+		}
+		log.Println("[DB] error GetUserByUsername():", err)
+		return -1, err
+	}
+	return id, nil
+}
