e80ec6463250870fd8cbff57ae911ac68fa56089
0x0FACED <https.genius@gmail.com>
2024-08-19 16:44:48 +0300

    added: Start() func and small changes

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,49 +1,59 @@
 package server
 
 import (
+	"errors"
+	"net"
+
 	"github.com/0x0FACED/link-saver-api/config"
-	"github.com/0x0FACED/link-saver-api/internal/storage"
-	"github.com/0x0FACED/link-saver-api/internal/storage/postgres"
+	"github.com/0x0FACED/link-saver-api/internal/cached/redis"
+	"github.com/0x0FACED/link-saver-api/internal/service"
 	"github.com/0x0FACED/proto-files/link_service/gen"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"google.golang.org/grpc"
 )
 
 type server struct {
-	config config.ServerConfig
-	db     storage.Database
-	gen.UnimplementedLinkServiceServer
-
-	echo *echo.Echo
+	config  config.ServerConfig
+	service *service.LinkService
+	echo    *echo.Echo
 }
 
 func New(cfg *config.Config) *server {
-
-	// TODO: add more drivers
-	var db storage.Database
-	switch cfg.Database.Driver {
-	case "postgres":
-		db = &postgres.Postgres{Config: cfg.Database}
-	default:
-		db = &postgres.Postgres{Config: cfg.Database}
-	}
-
-	if db.Connect() == storage.ErrConnectDB {
-		panic("cant connect db, panic!")
-	}
-
+	r := redis.New(cfg.Redis)
+	s := service.New(cfg.Database, r)
 	return &server{
-		config: cfg.Server,
-		echo:   configureRouter(),
-		db:     db,
+		config:  cfg.Server,
+		echo:    echo.New(),
+		service: s,
 	}
 }
 
-func configureRouter() *echo.Echo {
-	e := echo.New()
+func Start() error {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		return errors.New("Failed to listen: " + err.Error())
+	}
+	s := grpc.NewServer()
+	cfg, err := config.Load()
+	if err != nil {
+		return errors.New("Failed to load config: " + err.Error())
+	}
+	srv := New(cfg)
+	srv.configureRouter()
+	gen.RegisterLinkServiceServer(s, srv.service)
+	if err := s.Serve(lis); err != nil {
+		return errors.New("Failed to serve: " + err.Error())
+	}
 
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	return nil
+}
 
-	return e
+func (s *server) configureRouter() {
+
+	s.echo.Use(middleware.Logger())
+	s.echo.Use(middleware.Recover())
+
+	// handler to return html page to user
+	s.echo.GET("/gen/:username/:url", s.serveLink)
 }
