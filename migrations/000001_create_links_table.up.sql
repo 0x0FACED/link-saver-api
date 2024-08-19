f0e4274fb8e094c95dc99135d7b12927bceb769a
0x0FACED <https.genius@gmail.com>
2024-08-19 16:48:23 +0300

    upd: migration v1 up

--- migrations/000001_create_links_table.up.sql
+++ migrations/000001_create_links_table.up.sql
@@ -1,8 +1,15 @@
+
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(32) NOT NULL UNIQUE
+);
+
 CREATE TABLE links (
     id BIGSERIAL PRIMARY KEY,
-    original_link UNIQUE TEXT NOT NULL,
-    username VARCHAR(32) NOT NULL
-    description VARCHAR(32) UNIQUE NOT NULL
-    link_path VARCHAR(128) NOT NULL
-    date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    original_url TEXT NOT NULL,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    description VARCHAR(32) NOT NULL,
+    content BYTEA NOT NULL,
+    date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (user_id, original_url)
 );
