05d720c7cac914b16a17f8720966cf20daea4c98
0x0FACED <https.genius@gmail.com>
2024-08-20 16:01:55 +0300

    added: logger and colly to `LinkService` struct

--- internal/service/service.go
+++ internal/service/service.go
@@ -3,19 +3,33 @@ package service
 import (
 	"github.com/0x0FACED/link-saver-api/config"
 	"github.com/0x0FACED/link-saver-api/internal/cached/redis"
+	"github.com/0x0FACED/link-saver-api/internal/logger"
 	"github.com/0x0FACED/link-saver-api/internal/storage"
 	"github.com/0x0FACED/link-saver-api/internal/storage/postgres"
 	"github.com/0x0FACED/proto-files/link_service/gen"
+	"github.com/gocolly/colly"
+	"go.uber.org/zap"
 )
 
 type LinkService struct {
 	gen.UnimplementedLinkServiceServer
 
-	db    storage.Database
-	redis *redis.Redis
+	db     storage.Database
+	redis  *redis.Redis
+	Logger *logger.ZapLogger
+	colly  *colly.Collector
 }
 
-func New(cfg config.DatabaseConfig, redis *redis.Redis) *LinkService {
+func New(cfg config.DatabaseConfig, redis *redis.Redis, logger *logger.ZapLogger) *LinkService {
+	logger.Debug("Database config: ",
+		zap.String("db_name", cfg.Name),
+		zap.String("db_host", cfg.Host),
+		zap.String("db_port", cfg.Port),
+		zap.String("db_username", cfg.Username),
+		zap.String("db_password", cfg.Password),
+		zap.String("db_driver", cfg.Driver),
+	)
+
 	// TODO: add more drivers
 	var db storage.Database
 	switch cfg.Driver {
@@ -29,8 +43,15 @@ func New(cfg config.DatabaseConfig, redis *redis.Redis) *LinkService {
 		panic("cant connect db, panic!")
 	}
 
+	logger.Info("Successfully connected to database")
+
+	c := colly.NewCollector(
+		colly.Async(true),
+	)
 	return &LinkService{
-		db:    db,
-		redis: redis,
+		db:     db,
+		redis:  redis,
+		Logger: logger,
+		colly:  c,
 	}
 }
