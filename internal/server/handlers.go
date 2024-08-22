b48c447c7dd5753fb565c63ba4e35630f3d9a213
0x0FACED <https.genius@gmail.com>
2024-08-22 13:49:30 +0300

    upd: since using user id, added parseint to int64 from string

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -3,19 +3,21 @@ package server
 import (
 	"context"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"go.uber.org/zap"
 )
 
 func (s *server) serveLink(ctx echo.Context) error {
-	u := ctx.Param("username")
+	u := ctx.Param("user_id")
+	userId, _ := strconv.ParseInt(u, 10, 64)
 	url := ctx.Param("url")
 	s.service.Logger.Debug("Received serveLink() request with params",
 		zap.String("user", u),
 		zap.String("gen_url", url),
 	)
-	original, err := s.service.GetURLFromRedis(context.TODO(), u, url)
+	original, err := s.service.GetURLFromRedis(context.TODO(), userId, url)
 	if err != nil {
 		s.service.Logger.Error("Error GetURLFromRedis()",
 			zap.Error(err),
@@ -26,7 +28,7 @@ func (s *server) serveLink(ctx echo.Context) error {
 		zap.String("original_url", original),
 	)
 
-	content, err := s.service.GetContentFromDatabase(context.TODO(), u, original)
+	content, err := s.service.GetContentFromDatabase(context.TODO(), userId, original)
 	if err != nil {
 		s.service.Logger.Error("Error GetContentFromDatabase()",
 			zap.Error(err),
