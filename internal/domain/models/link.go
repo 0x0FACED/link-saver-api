a75f98f0523c5c9d06818dedc3083c8b765916a3
0x0FACED <https.genius@gmail.com>
2024-08-22 13:48:42 +0300

    upd: username -> user id

--- internal/domain/models/link.go
+++ internal/domain/models/link.go
@@ -5,7 +5,7 @@ import "time"
 type Link struct {
 	ID          int       `json:"id" db:"id"`
 	OriginalURL string    `json:"original_url" db:"original_url"`
-	UserName    string    `json:"username" db:"username" required:"true"`
+	UserID      int64     `json:"user_id" db:"telegram_user_id" required:"true"`
 	Description string    `json:"description" db:"description" required:"true"`
 	Content     []byte    `db:"content"`
 	DateAdded   time.Time `json:"date_added" db:"date_added"`
