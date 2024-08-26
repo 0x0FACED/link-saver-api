4bb75763f17470b1def7443bb9f1d415dd45ec0c
0x0FACED <https.genius@gmail.com>
2024-08-26 14:26:48 +0300

    upd: error wrapped

--- migrations/migrate.go
+++ migrations/migrate.go
@@ -2,25 +2,25 @@ package migrations
 
 import (
 	"errors"
-	"log"
 
+	"github.com/0x0FACED/link-saver-api/internal/wrap"
 	"github.com/golang-migrate/migrate"
 	_ "github.com/golang-migrate/migrate/database/postgres"
 	_ "github.com/golang-migrate/migrate/source/file"
 )
+
+var pkg = "migrations"
 
 func Up(url string) error {
 	m, err := migrate.New(
 		"file://./migrations/",
 		url)
 	if err != nil {
-		log.Fatalln("failed to create migration: ", err)
 		return err
 	}
 
 	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
-		log.Fatalln("failed to migrate up: ", err)
-		return err
+		return wrap.E(pkg, "failed to Up()", err)
 	}
 
 	return nil
